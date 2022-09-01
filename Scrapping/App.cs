6e6a47523f73a8aa5cece711abd336b046be3909
Igor Mucharski <igor.mucharski@crediok.app>
2022-09-01 16:23:56 -0300

    .

--- Scrapping/App.cs
+++ Scrapping/App.cs
@@ -1,6 +1,20 @@
-﻿namespace Scrapping;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Scrapping;
 
 public class App
 {
-    
+    private static readonly HttpClient Client = new ();
+
+    [FunctionName("WebScrappingTrigger")]
+    public static async Task Trigger([TimerTrigger("* */5 * * * *")] TimerInfo myTimer,
+        ILogger log)
+    {
+        await Client.GetAsync(new Uri("https://propertyscrapping.azurewebsites.net/function/trigger"));
+        await Client.GetAsync(new Uri("https://propertyscrapping.azurewebsites.net/scrapper/imovelweb"));
+    }
 }
